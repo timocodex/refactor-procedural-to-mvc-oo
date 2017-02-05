918e5b94b14c2ea69829f72079dea73b6168026e
timocodex <timothy.og.siahaan@gmail.com>
2017-02-05 23:58:29 +0700

    done refactoring

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-  $('#roller button.add').on('click',function(){
-    console.log('WAT')
-    $('.dice').append('<div class="die">0</div>')
-  })
-
-  $('roller button.roll').on('click',function(){
-    $('.die').each(function(k,die){
+let model = {
+    getRandomNumber : function(k,die){
       var value = Math.floor((Math.random()*6)+1)
       $(die).text(value)
-    })
-  })
-})
+    }
+}
+
+let controller ={
+    getEachDice : function(){
+      $('.die').each(model.getRandomNumber)
+    },
+    addDice: function(){
+      console.log('WAT')
+      $('.dice').append('<div class="die">0</div>')
+    }
+}
+
+let view = {
+  addnRoll: function(){
+    $('#roller button.add').on('click',controller.addDice)
+    $('roller button.roll').on('click',controller.getEachDice)
+  }
+}
+
+
+$(document).ready(view.addnRoll)
